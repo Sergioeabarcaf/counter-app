8f9556d43651e17610fd5159e68a26134b39984a
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-18 11:00:21 -0400

    FEAT: install enzyme and test with snapshot

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import CounterApp from '../CounterApp';
+import { shallow } from 'enzyme'
 
 describe('prueba sobre CounterApp', () => {
-    test('debe mostrar el mensaje "CounterApp"', () => {
+    // test('debe mostrar el mensaje "CounterApp"', () => {
+    //     const mensaje = 'CounterApp';
+    //     const { getByText } = render( <CounterApp />);
+    //     expect( getByText(mensaje) ).toBeInTheDocument();
+    // });
+
+    test('debe de mostrar el mensaje CounterApp', () => {
         const mensaje = 'CounterApp';
-        const { getByText } = render( <CounterApp />);
-        expect( getByText(mensaje) ).toBeInTheDocument();
+        const wrapper = shallow( <CounterApp />);
+
+        expect( wrapper ).toMatchSnapshot();
+
     });
 });
