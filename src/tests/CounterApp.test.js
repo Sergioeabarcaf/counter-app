55d502c2fd08efad61f0f87f2e9d751cd80d844c
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-18 14:07:53 -0400

    FEAT:  use beforeEach in unit test

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -10,17 +10,20 @@ describe('prueba sobre CounterApp', () => {
     //     expect( getByText(mensaje) ).toBeInTheDocument();
     // });
 
-    test('debe de renderizar la CounterApp', () => {
-        const wrapper = shallow( <CounterApp />);
+    const value = 100;
+    let wrapper = shallow( <CounterApp value={value}/> );
 
+    beforeEach( () => {
+        wrapper = shallow( <CounterApp value={value}/> );
+    });
+
+    test('debe de renderizar la CounterApp', () => {
         expect( wrapper ).toMatchSnapshot();
 
     });
 
     test('debe validar el mensaje de CounterApp en H1', () => {
         const mensaje = 'CounterApp';
-        const wrapper = shallow(<CounterApp />);
-
         const textoEnCounterApp = wrapper.find('h1').text();
 
         expect( textoEnCounterApp ).toBe(mensaje);  
@@ -28,11 +31,35 @@ describe('prueba sobre CounterApp', () => {
     });
 
     test('debe mostrar el valor por defecto de 100', () => {
-        const value = 100;
-        const wrapper = shallow( <CounterApp value={value}/> );
-
         const valueEnCounterApp = wrapper.find('h2').text();
         
         expect(valueEnCounterApp).toBe(value.toString());
-    })
+    });
+
+    test('debe incrementar el value con el botón +1', () => {
+
+        const btn1 = wrapper.find('button').at(0).simulate('click');
+        const valueEnCounterApp = wrapper.find('h2').text();
+
+        expect( valueEnCounterApp ).toBe((value + 1).toString());
+
+    });
+
+    test('debe decrecer el value con el botón -1', () => {
+
+        const btn1 = wrapper.find('button').at(2).simulate('click');
+        const valueEnCounterApp = wrapper.find('h2').text();
+
+        expect( valueEnCounterApp ).toBe((value - 1).toString());
+
+    });
+
+    test('debe resetear el value con el botón reset', () => {
+
+        const btn1 = wrapper.find('button').at(1).simulate('click');
+        const valueEnCounterApp = wrapper.find('h2').text();
+
+        expect( valueEnCounterApp ).toBe(value.toString());
+
+    });
 });
